40a4db902b195f4e99487cc196860af22611b514
Asif Bin Hossain <a.evan17@gmail.com>
2020-08-05 11:29:26 -0400

    code cleanup following DRY

--- script.js
+++ script.js
@@ -9,8 +9,18 @@ const image2 = document.getElementById('image2');                      /* 2nd Im
 const image3 = document.getElementById('image3');                      /* 3rd Image */
 const textBox = document.getElementById('text-box');                   /* Text box in Projects section */
 
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
+const COLOR_BLACK = 'rgb(0 0 0/50%)';
+const COLOR_WHITE = 'rgb(255 255 255/50%)';
+
+const ICON_MOON = 'fa-moon';
+const ICON_SUN = 'fa-sun';
+
 /*
  * Switch the images for given light/dark color
+ * @arg color : light or dark theme color 
  */
 function switchImageMode(color) {
     image1.src = `img/undraw_mind_map_${color}.svg`
@@ -19,40 +29,37 @@ function switchImageMode(color) {
 }
 
 /*
- * Switch to dark mode
+ * Toggle Light-Dark Mode theme color
+ * @arg themeColor : light or dark theme color
  */
-function switchToDarkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0/50%)';              // black
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';  // white
-    toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    switchImageMode('dark');
+function toggleThemeColor(themeColor) {
+    isLightMode = (themeColor === LIGHT_THEME) ? true : false;
+    nav.style.backgroundColor = isLightMode ? COLOR_WHITE : COLOR_BLACK;
+    textBox.style.backgroundColor = isLightMode ? COLOR_BLACK : COLOR_WHITE;
+    toggleIcon.children[0].textContent = isLightMode ? 'Light Mode' : 'Dark Mode';
 
+    if (isLightMode) {
+        toggleIcon.children[1].classList.replace(ICON_MOON, ICON_SUN);
+        switchImageMode(LIGHT_THEME);
+    } else {
+        toggleIcon.children[1].classList.replace(ICON_SUN, ICON_MOON);
+        switchImageMode(DARK_THEME);
+    }
 }
 
 /*
- * Switch to light mode
- */
-function switchToLightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255/50%)';  // white
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';  // black
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    switchImageMode('light');
-}
-
-/*
- * Switch Light-Dark theme dynamically
+ * Switch Light-Dark theme dynamically based on the event
  */
 function switchTheme(event) {
     if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        switchToDarkMode();
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        switchToLightMode();
+        document.documentElement.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME);
+        toggleThemeColor(DARK_THEME);
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', LIGHT_THEME);
+        localStorage.setItem('theme', LIGHT_THEME);
+        toggleThemeColor(LIGHT_THEME);
     }
 }
 
@@ -62,17 +69,22 @@ function switchTheme(event) {
  */
 toggleSwitch.addEventListener('change', switchTheme);
 
-// Check local storage for theme
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        switchToDarkMode();
-    }
-    else if (currentTheme === 'light') {
-        toggleSwitch.checked = false;
-        switchToLightMode();
-    }
+/*
+ * Restore previously saved theme from the local storage
+ */
+function restorePrevTheme(currentTheme) {
+    if (currentTheme === null)
+        return;
 
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === DARK_THEME) {
+        toggleSwitch.checked = true;
+        toggleThemeColor(DARK_THEME);
+    }
+    else if (currentTheme === LIGHT_THEME) {
+        toggleSwitch.checked = false;
+        toggleThemeColor(LIGHT_THEME);
+    }
 }
+restorePrevTheme(localStorage.getItem('theme'));
+
