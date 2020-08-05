5bca9ef1826efdf4fb43ab87a6148683ffade553
Asif Bin Hossain <a.evan17@gmail.com>
2020-08-05 10:39:29 -0400

    Fully functional light mode- dark mode website + some code-cleanup

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
+/*
+ * Get the Initial building-blocks first
+ */
+const toggleSwitch = document.querySelector('input[type="checkbox"]'); /* Toggle switch for Light-Dark Mode */
+const nav = document.getElementById('nav');                            /* Navigation Bar */
+const toggleIcon = document.getElementById('toggle-icon');             /* Toggle Sun/Moon icon for LD mode */
+const image1 = document.getElementById('image1');                      /* 1st Image */
+const image2 = document.getElementById('image2');                      /* 2nd Image */
+const image3 = document.getElementById('image3');                      /* 3rd Image */
+const textBox = document.getElementById('text-box');                   /* Text box in Projects section */
 
-// Get the Initial building-blocks first
-const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const nav = document.getElementById('nav');
-const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
-const textBox = document.getElementById('text-box');
+/*
+ * Switch the images for given light/dark color
+ */
+function switchImageMode(color) {
+    image1.src = `img/undraw_mind_map_${color}.svg`
+    image2.src = `img/undraw_setup_wizard_${color}.svg`
+    image3.src = `img/undraw_progressive_app_${color}.svg`
+}
 
-// Dark Mode Styles
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0/50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+/*
+ * Switch to dark mode
+ */
+function switchToDarkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0/50%)';              // black
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';  // white
     toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_mind_map_dark.svg'
-    image2.src = 'img/undraw_setup_wizard_dark.svg'
-    image3.src = 'img/undraw_progressive_app_dark.svg'
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    switchImageMode('dark');
+
 }
 
-// Light Mode Styles
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255/50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+/*
+ * Switch to light mode
+ */
+function switchToLightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255/50%)';  // white
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';  // black
     toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_mind_map_light.svg'
-    image2.src = 'img/undraw_setup_wizard_light.svg'
-    image3.src = 'img/undraw_progressive_app_light.svg'
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    switchImageMode('light');
 }
 
-// Switch theme dynamically
+/*
+ * Switch Light-Dark theme dynamically
+ */
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        localStorage.setItem('theme', 'dark');
+        switchToDarkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        localStorage.setItem('theme', 'light');
+        switchToLightMode();
     }
 }
 
-
-// Event-Listener
+/*
+ * Event-Listener
+ * Change Theme on switch toggle
+ */
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        switchToDarkMode();
+    }
+    else if (currentTheme === 'light') {
+        toggleSwitch.checked = false;
+        switchToLightMode();
+    }
+
+}
